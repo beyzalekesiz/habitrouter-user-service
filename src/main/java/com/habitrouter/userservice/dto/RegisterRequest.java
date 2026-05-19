3c72317033e45cc5651ea0c3999541c74cb2b6ef
beyzalekesiz <beyzalekesiz25@gmail.com>
2026-05-19 15:32:42 +0300

    created dto folder: added RegisterRequest.java, LoginRequest.java

---
+++ src/main/java/com/habitrouter/userservice/dto/RegisterRequest.java
@@ -0,0 +1,11 @@
+package com.habitrouter.userservice.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    
+    private String email;
+    private String password;
+    private String name;
+}
