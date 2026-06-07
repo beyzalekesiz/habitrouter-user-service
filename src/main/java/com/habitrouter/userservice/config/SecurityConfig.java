dc8fff49d3672facac2f7721ed1edeef35736ffd
beyzalekesiz <beyzalekesiz25@gmail.com>
2026-06-07 13:32:03 +0300

    created Security Configuration: fix password encoding, add security filter chain, hide password in response.

--- src/main/java/com/habitrouter/userservice/config/SecurityConfig.java
+++ src/main/java/com/habitrouter/userservice/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.habitrouter.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,6 +13,18 @@ public class SecurityConfig {
     @Bean //objects with one instance that spring manages throughout execution
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()) // tarayıcı tabanlı uygulamalar için
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll() //allow login, request for anyone
+                .anyRequest().authenticated() // any other request is locked
+            );
+        
+        return http.build();        
+
     }
     
 }
--- src/main/java/com/habitrouter/userservice/model/User.java
+++ src/main/java/com/habitrouter/userservice/model/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data; //getter, setter, toString(), equals(), hashCode()
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Data
 @Entity //this class represents a table
@@ -20,6 +22,7 @@ public class User {
     @Column(nullable = false)
     private String name;
 
+    @JsonIgnore //don't put this field in response, password musn't return to user
     @Column (nullable = false)
     private String password;
 
