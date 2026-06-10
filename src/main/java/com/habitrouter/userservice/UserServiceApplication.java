fa645c2a71ecd66c335de9b57404bc071acfcd52
beyzalekesiz <beyzalekesiz25@gmail.com>
2026-06-10 12:33:27 +0300

    added JwtService with token generation, changes in .env configuration

--- src/main/java/com/habitrouter/userservice/UserServiceApplication.java
+++ src/main/java/com/habitrouter/userservice/UserServiceApplication.java
@@ -3,10 +3,16 @@ package com.habitrouter.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		//add jwt secret as system property
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
--- src/main/java/com/habitrouter/userservice/service/JwtService.java
+++ src/main/java/com/habitrouter/userservice/service/JwtService.java
@@ -2,7 +2,35 @@ package com.habitrouter.userservice.service;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+
 @Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
     
+    @Value("${jwt.expiration}")
+    private long expiration; 
+
+    //create key: turn jwt code to bytes -> hash -> return key
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String email) {
+        Key key = getSigningKey();
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact(); //turn all into string
+    }
 }
