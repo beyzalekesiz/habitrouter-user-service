4fa95a11d63ecf0e7adb0c336419f32cd7a9e399
beyzalekesiz <beyzalekesiz25@gmail.com>
2026-06-11 13:52:09 +0300

    added method to extract email from token in JwtService

--- src/main/java/com/habitrouter/userservice/service/JwtService.java
+++ src/main/java/com/habitrouter/userservice/service/JwtService.java
@@ -2,16 +2,21 @@ package com.habitrouter.userservice.service;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
 import java.util.Date;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class JwtService {
+
 
     @Value("${jwt.secret}")
     private String secret;
@@ -24,13 +29,23 @@ public class JwtService {
         return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
+    //create token using the key
     public String generateToken(String email) {
-        Key key = getSigningKey();
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(key)
+                .signWith(getSigningKey())
                 .compact(); //turn all into string
+    }
+
+    public String extractEmail(String token){
+        Key key = getSigningKey();
+        return Jwts.parser() //to read tokens
+                .verifyWith((SecretKey) key) //check if token is signed: type cast the key to SecretKey
+                .build()
+                .parseSignedClaims(token) //decrypt and validate token
+                .getPayload() //get the data in token
+                .getSubject(); //get email
     }
 }
